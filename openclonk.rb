8487a47eb173eec369dcdf7353d4e34e22428704
Martin Plicht <mardmol@gmx.de>
2015-06-12 19:56:37 +0200

    openclonk: Accidentally bottle do
    That was not intentional.

--- openclonk.rb
+++ openclonk.rb
@@ -1,9 +1,18 @@
 class Openclonk < Formula
   head "https://github.com/openclonk/openclonk", :using => :git
-  url "https://github.com/openclonk/openclonk/archive/v6.1.tar.gz"
   homepage "http://www.openclonk.org"
+  url "https://github.com/openclonk/openclonk/archive/v6.1.tar.gz"
   sha256 "4e2e6cefedd4a13523593b285af23b530caa3a98ff02ac4adb215cf32889cb94"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-games"
+    cellar :any
+    sha256 "3e61a12dca0998d1c0a78466569abb325c32a26549034b1569cb187f479082d0" => :yosemite
+    sha256 "76e83695bfa80b4387004961a6693d555993a4a603cdcd386908edfbd195592a" => :mavericks
+    sha256 "f31c5f4100c5bd8ab22ca8d25dbf52df5b40ffbaed1567f1cb325547fcafcf2a" => :mountain_lion
+  end
+
+  depends_on :macos => :mountain_lion
   depends_on "cmake" => :build
   depends_on "jpeg"
   depends_on "libpng"
